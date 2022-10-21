114cdce54ec81a61972fbac66b136626ccd87bb4
Cao Thanh Danh <caothanhdanh.developer.1602@gmail.com>
2022-10-21 17:05:17 +0700

    feature/student-crud-Danhtest  Create Routes test

--- bktel/routes/web.php
+++ bktel/routes/web.php
@@ -20,3 +20,26 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Response string
+Route::get('/user', function () {
+    return "Hello Thanh Danh";
+});
+
+// Response array
+Route::get('/foods', function () {
+    return ["sushi", "sashimi", "tofu"];
+});
+
+// Response an object
+Route::get('/aboutMe', function () {
+    return response() -> json([
+        'name' => 'Cao Thanh Danh',
+        'email' => 'hocsinhdanh@gmail.com'
+    ]);
+});
+
+// Response another request = redirect
+Route::get('/something', function () {
+    return redirect('/aboutMe');
+});
